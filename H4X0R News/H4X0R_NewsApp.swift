eeffe587fe34027e50dfb4b4248f5383a119a803
Melissa Elliston-Boyes <melissa.elliston-boyes@trademe.co.nz>
2024-11-25 15:15:17 +1300

    Initial Commit

---
+++ H4X0R News/H4X0R_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4X0R_NewsApp.swift
+//  H4X0R News
+//
+//  Created by Melissa Elliston-Boyes on 25/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct H4X0R_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
